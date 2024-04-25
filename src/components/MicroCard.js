92f5d512179af360a3f65ba6132ea0b19a47586c
Vasudev Garg <vasudevgarg@Vasudevs-MacBook-Pro.local>
2024-04-25 07:43:03 +0530

    gropdown started working now apply css to make it centered

--- src/components/MicroCard.js
+++ src/components/MicroCard.js
@@ -7,15 +7,19 @@ import Button from '@mui/material/Button';
 import SelectSearch from 'react-select-search';
 
 const MicroCard = ({title, author, cover_image, id}) => {
+    const [value, setValue]= useState("");
+    const [dropdown, setDropdown]= useState([]);
     const [issued, setIssued]= useState(false);
     const [processing, setProcessing]= useState(false);
+    const val= false;
   const navigate= useNavigate();
   const dispatch= useDispatch();
   const users= useSelector(e=>e.manageUsers);
 
   useEffect(()=>{
-    fetch("http://localhost:5002/user/allusers").then(e=>e.json()).then(e=>dispatch(addUsers(e)));
-  })
+    fetch("http://localhost:5002/user/allusers").then(e=>e.json()).then(e=>{e.map(ei=>dispatch(addUsers(e))); console.log(e);});
+  },[val]);
+
 
   const handleIssueBook=()=>{
         setIssued(true);
@@ -38,7 +42,11 @@ const MicroCard = ({title, author, cover_image, id}) => {
 
     }
     {
-        processing && <SelectSearch value={users.filter((e)=>e.usernam==="username")} name="language" placeholder="Search User" />
+        processing && 
+        
+        <><input onChange={e=>{setDropdown(users.filter(x=>x.username===e.target.value))}}/>
+        <div className="dropdown">{dropdown.map((e)=><div>{e.username}</div>)}{ console.log(dropdown)}</div>
+        </>
     }
 
 </div>
